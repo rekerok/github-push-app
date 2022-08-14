f305158164889a927e0e4e3da04a6e6d483f8fa4
rekerok <rekerok05@gmail.com>
2022-08-14 20:57:25 +0300

    add function which push(with update) several file

--- script/main.py
+++ script/main.py
@@ -1,5 +1,23 @@
+import faker
 import github
 import os
+
+def updateSeveralFiles(repo, commit_message, file_list):
+    fake = faker.Faker()
+    master_ref = repo.get_git_ref("heads/main")
+    master_sha = master_ref.object.sha
+    base_tree = repo.get_git_tree(master_sha)
+    element_list = list()
+    for file in file_list:
+        print(repo.get_contents(file).decoded_content.decode())
+        element = github.InputGitTreeElement(file, "100644", "blob",
+                                             f"{repo.get_contents(file).decoded_content.decode()}\n{fake.paragraph(nb_sentences=1)}")
+        element_list.append(element)
+    tree = repo.create_git_tree(element_list, base_tree)
+    parent = repo.get_git_commit(master_sha)
+    commit = repo.create_git_commit(commit_message, tree, [parent])
+    master_ref.edit(commit.sha)
+
 
 def main():
     print("def main")
