8f003e97e7dbcc8ec8d7f7c520f4fb017c099bae
rekerok <rekerok05@gmail.com>
2022-08-26 17:27:02 +0300

    add function makeDictionaryRepo and searcFilesInDirectory
    add try/except to function readTokensFile. Write output json.

--- script/makeJson.py
+++ script/makeJson.py
@@ -1,6 +1,31 @@
 import json
 import github
-import github.GithubException
+
+
+# Repository = Directory
+def searcFilesInDirectory(repo, path=""):
+    list_content = list()
+    try:
+        contents = repo.get_contents(path)
+    except github.GithubException:
+        return []
+    print(repo.fork)
+    for file_contenet in contents:
+        if file_contenet.type == "dir":
+            list_content.extend(searcFilesInDirectory())
+        else:
+            list_content.append(file_contenet.path)
+    return list_content
+
+
+def makeDictionaryRepo(repos):
+    repos_dict = {}
+    for repo in repos:
+        repos_dict[repo.name] = {
+            "use": "Input True or False",
+            "files": [] if repo.fork else searcFilesInDirectory(repo)
+        }
+    return repos_dict
 
 
 def readTokensFile():
@@ -13,8 +38,9 @@ def readTokensFile():
         try:
             config_dict[value] = {
                 "username": user.login,
+                "use": True,
                 "url": user.html_url,
-                "repositories": list(repo.html_url for repo in user.get_repos())
+                "repositories": makeDictionaryRepo(user.get_repos())
             }
         except github.BadCredentialsException:
             print(f"Wrong token entered in account: {key}\n")
