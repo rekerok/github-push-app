f2393af68b436262d59e3113ec3ceec30705a639
rekerok <rekerok05@gmail.com>
2023-01-16 22:45:43 +0300

    move the config to a separate file and update gitignore

---
+++ config.py
@@ -0,0 +1,6 @@
+import os.path
+
+PATH_ACTIVE_ACCOUNTS = "account/active/"
+PATH_WAIT_ACCOUNTS = "account/wait/"
+PATH_CODE_STORAGE = "resources/"
+PATH_CODE_STORAGE_RANDOM = os.path.join(PATH_CODE_STORAGE, "random_code/")
